0e15e504956aa1e43b42f6be901d97228943d436
Sebastian Montoya <seb246@gmail.com>
2021-04-10 17:34:08 -0400

    added basic home page and incorporated react router dom

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from "./logo.svg";
 import "./App.css";
 import AddPhoto from "./components/photos/AddPhoto";
 import axios from "axios";
+import { Switch, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <AddPhoto />
+      <Link to="/addphoto">Add Photo</Link>
+
+      <Switch>
+        <Route exact path="/" render={(props) => <Home {...props} />} />
+        <Route
+          exact
+          path="/addphoto"
+          render={(props) => <AddPhoto {...props} />}
+        />
+      </Switch>
     </div>
   );
 }
